d6a2780bd407f893602783f01bf99af0939a2eaf
Rafal Dawid Lenartowicz <101730999+rafdlen@users.noreply.github.com>
2022-07-29 14:54:32 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://e830a51e.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
